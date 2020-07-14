c440f37289d6cf80ad60789d27381d0b63f2ffa0
Cantando <65988365+Cantando@users.noreply.github.com>
2020-07-13 22:47:44 -0400

    created back end delete method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { v1: uuidv1 } = require('uuid');
 const path = require("path");
 const fs = require("fs");
 const { log } = require("console");
+const { stringify } = require("querystring");
 
 
 
@@ -10,15 +11,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
 
 // html routes
-app.get ("/notes",(req,res)=>{
-   res.sendFile(path.join(__dirname,"public/notes.html"));
+app.get("/notes", (req, res) => {
+   res.sendFile(path.join(__dirname, "public/notes.html"));
 
 });
 
@@ -27,33 +28,56 @@ app.get ("/notes",(req,res)=>{
 // });
 
 // api routes
-app.get ("/api/notes",(req,res)=>{
-   fs.readFile("db/db.json",function (error, data){
+app.get("/api/notes", (req, res) => {
+   fs.readFile("db/db.json", function (error, data) {
       if (error) throw error;
       res.json(JSON.parse(data));
    });
 });
 
 // post method
-app.post ("/api/notes",(req,res)=>{
-   const noteObj =req.body;
-   noteObj.id = uuidv1(); 
+app.post("/api/notes", (req, res) => {
+   const noteObj = req.body;
+   noteObj.id = uuidv1();
    console.log(noteObj);
-   
-   fs.readFile("db/db.json",function (error, data){
+
+   fs.readFile("db/db.json", function (error, data) {
       if (error) throw errow;
-     var existingNotes = JSON.parse(data);
-     existingNotes.push(noteObj);
-     console.log(existingNotes);
-      
-      
-      fs.writeFile("db/db.json", JSON.stringify(existingNotes, null, "\t"),function(error){
+      var existingNotes = JSON.parse(data);
+      existingNotes.push(noteObj);
+      console.log(existingNotes);
+
+
+      fs.writeFile("db/db.json", JSON.stringify(existingNotes, null, "\t"), function (error) {
          if (error) throw error;
          return res.json(existingNotes);
       })
    });
 });
 
+// delete Method
+app.delete("/api/notes/:id", (req, res) => {
+   var chosenNote = req.params.id;
+   fs.readFile("db/db.json", function (error, data) {
+      if (error) throw error;
+      var existingNotes = JSON.parse(data);
+      var newNote = function () {
+         for (let index = 0; index < existingNotes.length; index++) {
+            if (chosenNote == existingNotes[index].id) {
+               var newArray = existingNotes.splice(index, 1);
+               return newArray;
+            }
+
+         }
+
+      }
+      newNote();
+      fs.writeFile("db/db.json", JSON.stringify(existingNotes, null, "\t"), function (error) {
+         if (error) throw errow;
+         return res.json(existingNotes);
+      })
+   })
+})
 
 
 
@@ -67,7 +91,9 @@ app.post ("/api/notes",(req,res)=>{
 
 
 
-app.listen(PORT,() =>
-{ console.log("App listner on port: "+ PORT );
+
+
+app.listen(PORT, () => {
+   console.log("App listner on port: " + PORT);
 
 })
